fd0cd0dff9e8df02b684346542499c7345969b5c
SerEgo <Frolov27101993@yandex.ru>
2025-09-06 15:59:45 +0300

    поменял язык в админ панели на русский и вывод имен моделей

--- CatAndDog/news/models.py
+++ CatAndDog/news/models.py
@@ -12,21 +12,29 @@ class Users(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField('Имя категории', max_length=30, unique=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
 
 class Post(models.Model):
     author = models.ForeignKey(Users, on_delete=models.CASCADE)
     time = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=50, default='Заголовок')
+    title = models.CharField('Заголовок', max_length=50, default='')
     category = models.ForeignKey(Category, default= 1, on_delete=models.CASCADE)
     text = CKEditor5Field()
 
     def get_absolute_url(self):
         return reverse('post_detail', args=[str(self.id)])
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
 
 
 class Comment(models.Model):
@@ -41,4 +49,8 @@ class Pets(models.Model):
     age = models.DateTimeField()
     text = models.TextField()
     photo = CKEditor5Field()
+
+    class Meta:
+        verbose_name = 'Питомец'
+        verbose_name_plural = 'Питомцы'
 
