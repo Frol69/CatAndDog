c4abb69a9b9b7283cb068b81ee2986f07a6053e1
Frol69 <Frolov27101993@yandex.ru>
2025-12-04 21:19:05 +0300

    доп к прошлому коммиту

--- news/forms.py
+++ news/forms.py
@@ -11,7 +11,7 @@ from django.utils.deconstruct import deconstructible
 #     code = 'russian'
 #
 #     def __init__(self, message=None):
-#         self.message = message if message else 'Должны присутствовать только русские символы, дефис и пробел.'
+#         self.message = message if message else 'Должны присутствовать только русские символы, цифры, дефис и пробел.'
 #
 #     def __call__(self, value, *args, **kwargs):
 #         if not (set(value) <= set(self.ALLOWED_CHARS)):
